188b5f0afcacbbf0e2fe6abf92c4863faae02646
Adrien Thebo <git@somethingsinistral.net>
2012-12-12 15:29:32 -0800

    (portage-#20) Expand portage validation
    This adds independent validation checks for package names and versions.
    The version format has been modified slightly to join the comparison and
    version string together.

--- lib/puppet/util/portage.rb
+++ lib/puppet/util/portage.rb
@@ -1,14 +1,14 @@
 module Puppet::Util::Portage
   # Util methods for Portage types and providers.
+  #
+  # @see http://dev.gentoo.org/~zmedico/portage/doc/man/ebuild.5.html 'man 5 ebuild section DEPEND'
 
   # Determine if a string is a valid DEPEND atom
   #
-  # @param atom [String] The string to validate as a DEPEND atom
+  # @param [String] atom The string to validate as a DEPEND atom
   #
   # @return [TrueClass]
   # @return [FalseClass]
-  #
-  # @see http://www.linuxmanpages.com/man5/ebuild.5.php#lbAE 'man 5 ebuild section DEPEND'
   def self.valid_atom?(atom)
     atom_prefix  = '(?:[<>=]|[<>]=)'
     atom_name    = '(?:[a-zA-Z-]+/[a-zA-Z-]+?)'
@@ -20,5 +20,38 @@ module Puppet::Util::Portage
 
     # Normalize the regular expression output to a boolean
     !!(atom =~ depend)
+  end
+
+  # Determine if a string is a valid package name
+  #
+  # @param [String] package_name the string to validate as a package name
+  #
+  # @return [TrueClass]
+  # @return [FalseClass]
+  def self.valid_package?(package_name)
+    package_pattern  = '(?:[\w-]+/(:?[\w-]+\w)?)'
+
+    regex = Regexp.new(%[^#{package_pattern}$])
+
+    !!(package_name =~ regex)
+  end
+
+  # Determine if a string is a valid DEPEND atom version
+  #
+  # This validates a standalone version string. The format is an optional
+  # comparator and a version string.
+  #
+  # @param [String] version_str The string to validate as a version string.
+  #
+  # @return [TrueClass]
+  # @Return [FalseClass]
+  def self.valid_version?(version_str)
+
+    compare_pattern = '(?:[<>=~]|[<>]=)'
+    version_pattern = '(?:[\d.]+[\w-]+)'
+
+    regex = Regexp.new(%[^#{compare_pattern}?#{version_pattern}$])
+
+    !!(version_str =~ regex)
   end
 end
--- spec/unit/util/portage_spec.rb
+++ spec/unit/util/portage_spec.rb
@@ -2,7 +2,7 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_helper"))
 
 describe Puppet::Util::Portage do
-  describe "when validating atoms" do
+  describe ".valid_atom?" do
 
     valid_atoms   = %w{=sys-devel/gcc-4.3.2-r4 >=app-crypt/gnupg-1.9 net-analyzer/nagios-nrpe}
     invalid_atoms = %w{sys-devel-gcc =sys-devel/gcc}
@@ -16,6 +16,74 @@ describe Puppet::Util::Portage do
     invalid_atoms.each do |atom|
       it "should reject #{atom} as an invalid name" do
         Puppet::Util::Portage.valid_atom?(atom).should be_false
+      end
+    end
+  end
+
+  describe ".valid_package?" do
+    valid_packages = [
+      'app-accessibility/brltty',
+      'dev-libs/userspace-rcu',
+      'sys-dev/gcc',
+    ]
+
+    invalid_packages = [
+      'app-accessibility/brltty-',
+      'gcc',
+      'sys-dev-gcc',
+      '=app-admin/eselect-fontconfig-1.1',
+    ]
+
+    valid_packages.each do |package|
+      it "should accept #{package} as valid" do
+        Puppet::Util::Portage.valid_package?(package).should be_true
+      end
+    end
+
+    invalid_packages.each do |package|
+      it "should reject #{package} as invalid" do
+        Puppet::Util::Portage.valid_package?(package).should be_false
+      end
+    end
+  end
+
+  describe "valid_version?" do
+    comparators = %w{~ < > = <= >=}
+
+    valid_versions = [
+      '4.3.2-r4',
+      '1.3',
+      '0.5.2_pre20120527',
+      '3.0_alpha12',
+    ]
+
+    invalid_versions = [
+      '!4.3.2-r4',
+      '4.2.3>',
+      'alpha',
+      'alpha-2.4.1'
+    ]
+
+    valid_versions.each do |ver|
+      it "should accept #{ver} as valid" do
+        Puppet::Util::Portage.valid_version?(ver).should be_true
+      end
+    end
+
+    describe 'with comparators' do
+      comparators.each do |comp|
+        valid_versions.each do |ver|
+          ver_str = comp + ver
+          it "should accept #{ver_str} as valid" do
+            Puppet::Util::Portage.valid_version?(ver_str).should be_true
+          end
+        end
+      end
+    end
+
+    invalid_versions.each do |ver|
+      it "should reject #{ver} as invalid" do
+        Puppet::Util::Portage.valid_version?(ver).should be_false
       end
     end
   end
