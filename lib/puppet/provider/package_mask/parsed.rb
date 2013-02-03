c3da47e4f56e4a4c65727fe73ad2d87b8ae2894d
Adrien Thebo <git@somethingsinistral.net>
2013-02-03 00:00:29 -0800

    (portage-#20) Implement version parsing for mask/unmask

--- lib/puppet/provider/package_mask/parsed.rb
+++ lib/puppet/provider/package_mask/parsed.rb
@@ -1,5 +1,5 @@
-# Trivial provider for managing a list of depend atoms to mask
 require 'puppet/provider/portagefile'
+require 'puppet/util/portage'
 
 Puppet::Type.type(:package_mask).provide(:parsed,
   :parent => Puppet::Provider::ParsedFile,
@@ -8,5 +8,23 @@ Puppet::Type.type(:package_mask).provide(:parsed,
 ) do
 
   desc "The package_mask provider backed by parsedfile"
-  record_line :parsed, :fields => %w{name}, :rts => true
+  record_line :parsed, :fields => %w{name}, :rts => true do |line|
+    hash = {}
+    if (match = line.match /^(\S+)\s*/)
+      # just a package
+      components = Puppet::Util::Portage.parse_atom(match[1])
+
+      # Try to parse version string
+      if components[:compare] and components[:version]
+        v = components[:compare] + components[:version]
+      end
+
+      hash[:name]    = components[:package]
+      hash[:version] = v
+    else
+      raise Puppet::Error, "Could not match '#{line}'"
+    end
+
+    hash
+  end
 end
