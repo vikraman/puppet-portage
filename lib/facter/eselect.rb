b05594f2d497ef4f342846a5925168db90ca2e14
Theo Chatzimichos <tampakrap@gentoo.org>
2013-02-15 13:46:15 +0100

    Add more eselect modules as facts Some eselect modules are blacklisted, since they either don't have an `eselect $module show` command, or they don't produce useful output

--- lib/facter/eselect.rb
+++ lib/facter/eselect.rb
@@ -1,7 +1,16 @@
-# make_profile.rb
-Facter.add("make_profile") do
-  confine :operatingsystem => :gentoo
-  setcode do
-    %x{eselect --brief --no-color profile show}.strip
+eselect_modules = %x(eselect modules list).split("  ").reject! { |c| c.empty? }
+eselect_modules = eselect_modules.delete_if { |c| c["\n"] }
+eselect_modules_blacklist = [
+  "help", "usage", "version", "bashcomp", "env", "fontconfig", "modules",
+  "news", "rc",
+]
+eselect_modules = eselect_modules - eselect_modules_blacklist
+
+eselect_modules.each do |eselect_module|
+  Facter.add("eselect_#{eselect_module}") do
+    confine :operatingsystem => :gentoo
+    setcode do
+      %x{eselect --brief --no-color #{eselect_module} show}.strip.split(' ')[0]
+    end
   end
 end
