f186637a668a34792943acdc7e1644da88ff7c1c
Adrien Thebo <git@somethingsinistral.net>
2013-02-16 12:52:46 -0800

    (portage-#38) Stub out File.chmod and Dir.mkdir
    specs fail when /etc/portage/package.use/* files cannot be chmodded.
    This isn't the best location for these stubs, but right now they're
    pretty tightly integrated into the portagefile provider.  Since the
    portagefile provider is pretty universal across these specs, this will
    do for now.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,14 @@ require 'puppet'
 
 RSpec.configure do |config|
   config.mock_with :mocha
-end
 
+  # (portage-#38) specs fail when /etc/portage/package.use/* files cannot be
+  # chmodded. This isn't the best location for these stubs, but right now
+  # they're pretty tightly integrated into the portagefile provider. Since
+  # the portagefile provider is pretty universal across these specs, this will
+  # do for now.
+  config.before do
+    Dir.stubs(:mkdir)
+    File.stubs(:chmod)
+  end
+end
