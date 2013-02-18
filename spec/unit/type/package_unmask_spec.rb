c729b473ed911e6328aa291416f58fe408c78d84
Adrien Thebo <git@somethingsinistral.net>
2013-02-17 22:09:33 -0800

    Clean up type specs to use rspec described_class helper

--- spec/unit/type/package_unmask_spec.rb
+++ spec/unit/type/package_unmask_spec.rb
@@ -1,15 +1,12 @@
 require 'spec_helper'
 
-type_class = Puppet::Type.type(:package_unmask)
-
-describe type_class do
+describe Puppet::Type.type(:package_unmask) do
   before do
-    @class    = type_class
     @provider = stub 'provider'
     @provider.stubs(:name).returns(:parsed)
     @provider.stubs(:ancestors).returns([Puppet::Provider::ParsedFile])
     @provider.stubs(:default_target).returns("defaulttarget")
-    @class.stubs(:defaultprovider).returns(@provider)
+    described_class.stubs(:defaultprovider).returns(@provider)
   end
 
   describe "when validating attributes" do
@@ -18,32 +15,32 @@ describe type_class do
 
     params.each do |param|
       it "should have the #{param} param" do
-        @class.attrtype(param).should == :param
+        described_class.attrtype(param).should == :param
       end
     end
 
     properties.each do |property|
       it "should have the #{property} property" do
-        @class.attrtype(property).should == :property
+        described_class.attrtype(property).should == :property
       end
     end
   end
 
   it "should have name as the namevar" do
-    @class.key_attributes.should == [:name]
+    described_class.key_attributes.should == [:name]
   end
 
   describe "when validating the target property" do
     it "should default to the provider's default target" do
-      @class.new(:name => "sys-devel/gcc").should(:target).should == "/etc/portage/package.unmask/defaulttarget"
+      described_class.new(:name => "sys-devel/gcc").should(:target).should == "/etc/portage/package.unmask/defaulttarget"
     end
 
     it "should munge targets that do not specify a fully qualified path" do
-      @class.new(:name => "sys-devel/gcc", :target => "gcc").should(:target).should == "/etc/portage/package.unmask/gcc"
+      described_class.new(:name => "sys-devel/gcc", :target => "gcc").should(:target).should == "/etc/portage/package.unmask/gcc"
     end
 
     it "should not munge fully qualified targets" do
-      @class.new(:name => "sys-devel/gcc", :target => "/tmp/gcc").should(:target).should == "/tmp/gcc"
+      described_class.new(:name => "sys-devel/gcc", :target => "/tmp/gcc").should(:target).should == "/tmp/gcc"
     end
   end
 end
--- spec/unit/type/package_use_spec.rb
+++ spec/unit/type/package_use_spec.rb
@@ -1,15 +1,12 @@
 require 'spec_helper'
 
-type_class = Puppet::Type.type(:package_use)
-
-describe type_class do
+describe Puppet::Type.type(:package_use) do
   before do
-    @class    = type_class
     @provider = stub 'provider'
     @provider.stubs(:name).returns(:parsed)
     @provider.stubs(:ancestors).returns([Puppet::Provider::ParsedFile])
     @provider.stubs(:default_target).returns("defaulttarget")
-    @class.stubs(:defaultprovider).returns(@provider)
+    described_class.stubs(:defaultprovider).returns(@provider)
   end
 
   describe "when validating attributes" do
@@ -18,46 +15,46 @@ describe type_class do
 
     params.each do |param|
       it "should have the #{param} param" do
-        @class.attrtype(param).should == :param
+        described_class.attrtype(param).should == :param
       end
     end
 
     properties.each do |property|
       it "should have the #{property} property" do
-        @class.attrtype(property).should == :property
+        described_class.attrtype(property).should == :property
       end
     end
   end
 
   it "should have name as the namevar" do
-    @class.key_attributes.should == [:name]
+    described_class.key_attributes.should == [:name]
   end
 
   describe "when validating the use property" do
     it "should accept a string for use" do
-      lambda { @class.new(:name => "sys-devel/gcc", :use => "openmp") }.should_not raise_error
+      lambda { described_class.new(:name => "sys-devel/gcc", :use => "openmp") }.should_not raise_error
     end
 
     it "should reject use flags with a space" do
-      lambda { @class.new(:name => "sys-devel/gcc", :use => "open mp") }.should raise_error
+      lambda { described_class.new(:name => "sys-devel/gcc", :use => "open mp") }.should raise_error
     end
 
     it "should accept an array for use" do
-      lambda { @class.new(:name => "sys-devel/gcc", :use => ["doc","openmp"]) }.should_not raise_error
+      lambda { described_class.new(:name => "sys-devel/gcc", :use => ["doc","openmp"]) }.should_not raise_error
     end
   end
 
   describe "when validating the target property" do
     it "should default to the provider's default target" do
-      @class.new(:name => "sys-devel/gcc").should(:target).should == "/etc/portage/package.use/defaulttarget"
+      described_class.new(:name => "sys-devel/gcc").should(:target).should == "/etc/portage/package.use/defaulttarget"
     end
 
     it "should munge targets that do not specify a fully qualified path" do
-      @class.new(:name => "sys-devel/gcc", :target => "gcc").should(:target).should == "/etc/portage/package.use/gcc"
+      described_class.new(:name => "sys-devel/gcc", :target => "gcc").should(:target).should == "/etc/portage/package.use/gcc"
     end
 
     it "should not munge fully qualified targets" do
-      @class.new(:name => "sys-devel/gcc", :target => "/tmp/gcc").should(:target).should == "/tmp/gcc"
+      described_class.new(:name => "sys-devel/gcc", :target => "/tmp/gcc").should(:target).should == "/tmp/gcc"
     end
   end
 end
