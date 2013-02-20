d643dcc9dc61d7324459d7e75146199e51569521
Adrien Thebo <git@somethingsinistral.net>
2013-02-19 22:57:49 -0800

    Add package_keywords coverage for version property

--- spec/integration/provider/package_keywords/parsed_spec.rb
+++ spec/integration/provider/package_keywords/parsed_spec.rb
@@ -17,33 +17,66 @@ describe Puppet::Type.type(:package_keywords).provider(:parsed) do
 
   subject { File.read(path) }
 
-  describe "a single instance" do
-    describe "with a single keyword" do
-      let(:resources) do
-        [] << type_class.new(
-          :name     => 'app-admin/dummy',
-          :keywords => '~amd64',
-          :target   => path,
-          :provider => :parsed
-        )
+  describe "with a single instance" do
+    describe "without a version" do
+      describe "with a single keyword" do
+        let(:resources) do
+          [] << type_class.new(
+            :name     => 'app-admin/dummy',
+            :keywords => '~amd64',
+            :target   => path,
+            :provider => :parsed
+          )
+        end
+
+        it { should have(1).lines }
+        it { should match %r[^app-admin/dummy ~amd64$] }
       end
 
-      it { should have(1).lines }
-      it { should match %r[^app-admin/dummy ~amd64$] }
+      describe "with multiple keywords" do
+        let(:resources) do
+          [] << type_class.new(
+            :name     => 'app-admin/dummy',
+            :keywords => ['~amd64', '~x86'],
+            :target   => path,
+            :provider => :parsed
+          )
+        end
+
+        it { should have(1).lines }
+        it { should match %r[^app-admin/dummy ~amd64 ~x86$] }
+      end
     end
+    describe "with a version" do
+      describe "with a single keyword" do
+        let(:resources) do
+          [] << type_class.new(
+            :name     => 'app-admin/dummy',
+            :keywords => '~amd64',
+            :target   => path,
+            :version  => '>=2.3.4-alpha1',
+            :provider => :parsed
+          )
+        end
 
-    describe "with multiple keywords" do
-      let(:resources) do
-        [] << type_class.new(
-          :name     => 'app-admin/dummy',
-          :keywords => ['~amd64', '~x86'],
-          :target   => path,
-          :provider => :parsed
-        )
+        it { should have(1).lines }
+        it { should match %r[^>=app-admin/dummy-2\.3\.4-alpha1 ~amd64$] }
       end
 
-      it { should have(1).lines }
-      it { should match %r[^app-admin/dummy ~amd64 ~x86$] }
+      describe "with multiple keywords" do
+        let(:resources) do
+          [] << type_class.new(
+            :name     => 'app-admin/dummy',
+            :keywords => ['~amd64', '~x86'],
+            :target   => path,
+            :version  => '>=2.3.4-alpha1',
+            :provider => :parsed
+          )
+        end
+
+        it { should have(1).lines }
+        it { should match %r[^>=app-admin/dummy-2\.3\.4-alpha1 ~amd64 ~x86$] }
+      end
     end
   end
 end
