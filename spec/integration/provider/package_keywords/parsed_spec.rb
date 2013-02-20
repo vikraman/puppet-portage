b5f5df3a29ae6e436af5857bd87f50a6f33135b6
Adrien Thebo <git@somethingsinistral.net>
2013-02-19 22:24:26 -0800

    Add basic integration coverate for package_keywords
    rspec is rad.

---
+++ spec/integration/provider/package_keywords/parsed_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:package_keywords).provider(:parsed) do
+
+  let(:path) { PuppetIntegration::TmpdirManager.instance.tmpfile }
+  let(:type_class) { Puppet::Type.type(:package_keywords) }
+
+  around(:each) do |example|
+    described_class.stubs(:header).returns ''
+
+    catalog = Puppet::Resource::Catalog.new
+    resources.each { |r| catalog.add_resource r }
+    catalog.apply
+    example.run
+    catalog.clear
+  end
+
+  subject { File.read(path) }
+
+  describe "a single instance" do
+    describe "with a single keyword" do
+      let(:resources) do
+        [] << type_class.new(
+          :name     => 'app-admin/dummy',
+          :keywords => '~amd64',
+          :target   => path,
+          :provider => :parsed
+        )
+      end
+
+      it { should have(1).lines }
+      it { should match %r[^app-admin/dummy ~amd64$] }
+    end
+
+    describe "with multiple keywords" do
+      let(:resources) do
+        [] << type_class.new(
+          :name     => 'app-admin/dummy',
+          :keywords => ['~amd64', '~x86'],
+          :target   => path,
+          :provider => :parsed
+        )
+      end
+
+      it { should have(1).lines }
+      it { should match %r[^app-admin/dummy ~amd64 ~x86$] }
+    end
+  end
+end
