3ab31bc5fc826660d92991666e4f0d8ff8e2bfce
Vikraman <vikraman@gentoo.org>
2013-09-09 00:52:37 +0530

    Add documentation for the module hash returned

--- lib/puppet/util/eselect.rb
+++ lib/puppet/util/eselect.rb
@@ -13,7 +13,13 @@ module Puppet::Util::Eselect
   #
   # @param [String] name The name of the module
   #
-  # @return [Hash] The module
+  # @return [Hash]:
+  #   * command [Symbol] The command used to manage this module
+  #   * flags [Array<String>] The flags to pass to the command
+  #   * param [String] The param to be passed after flags, or nil
+  #   * get [Array[String]] The flags to be passed to get the state of the module
+  #   * set [Array[String]] The flags to be passed to set the state of the module
+  #   * parse [Proc] The function to parse the command output
   def module(name)
     modules = {
       'gcc' => {
